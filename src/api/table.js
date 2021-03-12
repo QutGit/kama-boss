b52863a4b87884c350b87ac8cb4b6a6b15663f69
jiajia <jiajia@guanghe.tv>
2021-03-12 20:04:12 +0800

    feat: 🎸 kama后台初始化

---
+++ src/api/table.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+const baseUlr = 'http://10.8.201.39:3301'
+export function getCategorys() {
+  return request({
+    url: `${baseUlr}/bossapi/categorys`,
+    method: 'get'
+  })
+}
+export function createArticle(data) {
+  return request({
+    url: `${baseUlr}/bossapi/create`,
+    method: 'post',
+    data
+  })
+}
+
+export function getArticles(params) {
+  return request({
+    url: `${baseUlr}/bossapi/articles`,
+    method: 'get',
+    params
+  })
+}
+
+export function deleteArticle(params) {
+  return request({
+    url: `${baseUlr}/bossapi/delete`,
+    method: 'delete',
+    params
+  })
+}
